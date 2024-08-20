689f57faf9d6d7a407ac1b62182891dd59a40439
kento <kentotricking@icloud.com>
2024-08-20 18:49:25 +0900

    first commit

---
+++ src/main/java/raisetech/studentmanagement/StudentmanagementApplication.java
@@ -0,0 +1,21 @@
+package raisetech.studentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StudentmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmanagementApplication.class, args);
+	}
+
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello,World!";
+	}
+}
